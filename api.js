3506fb61140fdf511646e545fc607f0641bbf7f6
Lara Lays <lara.lays.bh6@gmail.com>
2022-12-10 21:16:32 -0300

    Criando servidor

---
+++ api.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello Word')
+})
+
+app.post('/usuario', (req, res) => {
+    console.log(req.body);
+    res.json({
+        "statucCode": 200
+    })
+})
+
+app.listen(3000);
