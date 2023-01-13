0a71ec07e4cb9bcd3ff9420a5208d92cd29aa3ae
Lara Lays <lara.lays.bh6@gmail.com>
2023-01-13 20:35:06 -0300

    Alterações pt 2

--- src/api.js
+++ src/api.js
@@ -1,18 +1,27 @@
 import express from 'express';
+import fs from 'fs';
+import https from 'https';
+import cors from "cors";
 
 const app = express();
 
-const port = process.env.PORT || 3000;
-
 app.use(express.json());
 
-import cors from "cors";
+// const  opitions = {
+//     origin: ["*"]
+// };
 
-const  opitions = {
-    origin: ["*"]
-};
 
-app.use(cors(opitions));
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
+// app.use(cors(opitions));
 
 import routerUsuario from '../routers/UsuarioRouter.js';
 import routerProdutos from '../routers/ProdutosRouter.js';
@@ -23,4 +32,9 @@ app.use(routerProdutos);
 app.use(routerCarrinho);
 
 
-app.listen(port);
+app.listen(3000);
+
+https.createServer({
+    cert: fs.readFileSync('src/SSL/code.crt'),
+    key: fs.readFileSync('src/SSL/code.key')
+},app).listen(3001, ()=> console.log("rodando em https"))
